db1f7fb26ad817b5ec0ab55616e79eac942fc7f6
dendendenden123 <159616363+dendendenden123@users.noreply.github.com>
2026-03-16 08:12:34 +0800

    add postman

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,11 @@
 import { authMiddleware } from "../middleware/authMiddleware.js";
-import { login, register, profile} from "../controller/authController.js"
+import { login, register, profile } from "../controller/authController.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/register", register);
-router.post("/login", (req, res)=>{ 
-    console.log("working")});
-    res.send("OK")
+router.post("/login", login);
 router.get("/profile", authMiddleware, profile);
 
 export default router;
